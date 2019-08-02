26b78685f2fc6101f365ec8b392e2171221099c1
Siddhant Gupta <siddhant@fasolutions.com>
2019-08-02 20:32:57 +0300

    lifeline

---
+++ js/db.js
@@ -0,0 +1,58 @@
+// Edit Data to show on homepage
+let articles = [
+    {
+        static: true,
+        icon: "fas fa-info",
+        color: "",
+        title: "About Me",
+        subtitle: "",
+        date: "",
+        tags: ["about", "branding"],
+        description: "Ever since I could first remember, I’ve been fascinated by how things work. While it took me some time to discover my passion for design, I haven’t stopped pursuing it since. I am incredibly detail orientated and believe that small thing can make the biggest impact. If I had to describe myself in one word, that’d be stalwart.",
+        role: "",
+        tech: [],
+        attachments: [{
+            name: "Siddhant Gupta",
+            image: "images/siddhantgupta.jpg",
+            link: "",
+        }],
+        actions: [{
+            name: "LinkedIn",
+            icon: "fab fa-linkedin",
+            link: "https://www.linkedin.com/in/guptasiddhant9/",
+        }, {
+            name: "GitHub",
+            icon: "fab fa-github",
+            link: "https://www.github.com/guptasiddhant/",
+        }, {
+            name: "Mail",
+            icon: "fas fa-envelope",
+            link: "mailto:me@guptasiddhant.com",
+        }, {
+            name: "Resume",
+            icon: "fas fa-file-download",
+            link: "#",
+            target: "_self"
+        }]
+    }, {
+        icon: "fas fa-bezier-curve",
+        color: "#000080",
+        title: "Business Espoo",
+        subtitle: "Stalwart Design",
+        date: "April 2019",
+        tags: ["project", "branding"],
+        description: "Business Espoo supports the vitality of businesses by offering the best, continuously developing services in one place.",
+        role: "Designed the Brand Identity and Style Guidelines",
+        tech: ["Sketch", "InDesign"],
+        attachments: [{
+            name: "BE Logo",
+            image: "images/BE.jpg",
+            link: "",
+        }],
+        actions: [{
+            name: "Read More",
+            icon: "fas fa-external-link-alt",
+            link: "#",
+        }]
+    },
+];
--- js/index.js
+++ js/index.js
@@ -1,74 +1,169 @@
 $(document).ready(function () {
-    loadSlider();
-    loadParticles();
+
+    let filters = [];
+    let allTags = [];
+
+    buildArticles(filters);
+
+    let filterArea = document.getElementById('filter-area');
+    filterArea.innerHTML = '';
+
+    articles.forEach((data) => {
+        data.tags.forEach((tag) => {
+            let found = false;
+            allTags.forEach((aTag) => {
+                if (aTag.name === tag) {
+                    aTag.count += 1;
+                    found = true;
+                }
+            });
+            if (!found) {
+                let tagsCounter = {name: tag, count: 1};
+                allTags.push(tagsCounter)
+            }
+        });
+    });
+
+    allTags.forEach((tag) => {
+        let button = document.createElement('a');
+        filterArea.appendChild(button);
+        button.className = "button-secondary";
+        let buttonText = document.createElement('span');
+        button.appendChild(buttonText);
+        buttonText.innerText = tag.name;
+        buttonText.style.textTransform = "Capitalize";
+        button.style.marginBottom = "0.5rem";
+        button.addEventListener('click', function (e) {
+            if (button.classList.contains('active')) {
+                button.classList.remove('active');
+                buttonText.innerText = tag.name;
+                filters = arrayRemove(filters, tag.name);
+            } else {
+                button.classList.add('active');
+                buttonText.innerText = tag.name + ' (' + tag.count.toString() + ')';
+                filters.push(tag.name);
+            }
+            console.log(filters);
+            buildArticles(filters);
+        });
+    });
+
+
 });
 
-
-function loadParticles() {
-    particlesJS("particles-js", {
-        "particles": {
-            "number": {"value": 100, "density": {"enable": true, "value_area": 800}},
-            "color": {"value": "#27ca03"},
-            "shape": {
-                "type": "edge",
-                "stroke": {"width": 0, "color": "#000000"},
-                "polygon": {"nb_sides": 6},
-                "image": {"src": "img/github.svg", "width": 100, "height": 100}
-            },
-            "opacity": {
-                "value": 0.5,
-                "random": false,
-                "anim": {"enable": false, "speed": 1, "opacity_min": 0.1, "sync": false}
-            },
-            "size": {
-                "value": 3,
-                "random": true,
-                "anim": {"enable": false, "speed": 40, "size_min": 0.1, "sync": false}
-            },
-            "line_linked": {"enable": true, "distance": 150, "color": "#ffffff", "opacity": 0.4, "width": 1},
-            "move": {
-                "enable": true,
-                "speed": 6,
-                "direction": "none",
-                "random": false,
-                "straight": false,
-                "out_mode": "out",
-                "bounce": false,
-                "attract": {"enable": false, "rotateX": 600, "rotateY": 1200}
-            }
-        },
-        "interactivity": {
-            "detect_on": "window",
-            "events": {
-                "onhover": {"enable": true, "mode": "repulse"},
-                "onclick": {"enable": false, "mode": "push"},
-                "resize": true
-            },
-            "modes": {
-                "grab": {"distance": 389.7982489531785, "line_linked": {"opacity": 1}},
-                "bubble": {"distance": 400, "size": 40, "duration": 2, "opacity": 8, "speed": 3},
-                "repulse": {"distance": 200, "duration": 0.4},
-                "push": {"particles_nb": 4},
-                "remove": {"particles_nb": 2}
-            }
-        },
-        "retina_detect": true
+function arrayRemove(arr, value) {
+    return arr.filter(function (ele) {
+        return ele !== value;
     });
-    // let count_particles, stats, update;
-    // stats = new Stats;
-    // stats.setMode(0);
-    // stats.domElement.style.position = 'absolute';
-    // stats.domElement.style.left = '0px';
-    // stats.domElement.style.top = '0px';
-    // document.body.appendChild(stats.domElement);
-    // count_particles = document.querySelector('.js-count-particles');
-    // update = function () {
-    //     stats.begin();
-    //     stats.end();
-    //     if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
-    //         count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
-    //     }
-    //     requestAnimationFrame(update);
-    // };
-    // requestAnimationFrame(update);
+}
+
+let dot = document.createElement('span');
+dot.className = "dot";
+
+function buildArticles(filters) {
+    let lifeline = document.getElementById('lifeline');
+    lifeline.innerHTML = '';
+
+    articles.forEach((data) => {
+        let matched = data.tags.some(r => filters.includes(r));
+
+        if (matched || filters.length === 0) {
+
+            let article = document.createElement('article');
+            lifeline.appendChild(article);
+
+            let articleIcon = document.createElement('div');
+            article.appendChild(articleIcon);
+            articleIcon.className = "article-icon";
+            if (data.color !== '') {
+                articleIcon.style.backgroundColor = data.color;
+            }
+            let iconTag = document.createElement('i');
+            articleIcon.appendChild(iconTag);
+            iconTag.className = data.icon;
+
+            let articleTitle = document.createElement('h2');
+            article.appendChild(articleTitle);
+            articleTitle.className = "article-title";
+            articleTitle.innerText = data.title;
+
+            if (!data.static) {
+                if (data.subtitle !== '') {
+                    let articleSubTitle = document.createElement('h4');
+                    article.appendChild(articleSubTitle);
+                    articleSubTitle.className = "article-subtitle";
+                    articleSubTitle.innerText = data.subtitle;
+                }
+                let articleTags = document.createElement('h5');
+                article.appendChild(articleTags);
+                articleTags.className = "article-tags";
+                if (data.date !== '') {
+                    articleTags.innerHTML = data.date;
+                    articleTags.appendChild(dot);
+                }
+                data.tags.forEach((tag, index) => {
+                    articleTags.innerHTML += tag;
+                    if (index + 1 !== data.tags.length) {
+                        articleTags.innerHTML += ", ";
+                    }
+                });
+            }
+
+            if (data.description !== '') {
+                let articleDesc = document.createElement('p');
+                article.appendChild(articleDesc);
+                articleDesc.className = "article-description";
+                articleDesc.innerText = data.description;
+            }
+
+            if (data.attachments !== []) {
+                let articleAttachments = document.createElement('div');
+                article.appendChild(articleAttachments);
+                articleAttachments.className = "article-attachments flex wrap";
+                data.attachments.forEach((attachment) => {
+                    let imageCard = document.createElement('a');
+                    articleAttachments.appendChild(imageCard);
+                    imageCard.className = "image-card";
+                    if (attachment.link !== '') {
+                        imageCard.href = attachment.link;
+                    }
+                    let image1 = document.createElement('img');
+                    imageCard.appendChild(image1);
+                    image1.src = attachment.image;
+                    image1.alt = attachment.name;
+                    image1.title = attachment.name;
+                    let image2 = document.createElement('img');
+                    imageCard.appendChild(image2);
+                    image2.src = attachment.image;
+                    image2.alt = attachment.name;
+                    image2.title = attachment.name;
+                    image1.onmouseover = function () {
+                        image2.style.display = "block";
+                    };
+                    image1.onmouseout = function () {
+                        image2.style.display = "none";
+                    };
+                });
+            }
+
+            if (data.actions !== []) {
+                let articleActions = document.createElement('div');
+                article.appendChild(articleActions);
+                articleActions.className = "article-actions flex wrap";
+                data.actions.forEach((action) => {
+                    let button = document.createElement('a');
+                    articleActions.appendChild(button);
+                    button.className = "button-secondary";
+                    button.href = action.link;
+                    button.target = action.target ? action.target : "_blank";
+                    let buttonIcon = document.createElement('i');
+                    button.appendChild(buttonIcon);
+                    buttonIcon.className = action.icon;
+                    let buttonText = document.createElement('span');
+                    button.appendChild(buttonText);
+                    buttonText.innerText = action.name;
+                });
+            }
+        }
+    });
 }
