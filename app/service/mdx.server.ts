8c6b6251d4cec1e8a421aa7a085a12e3c4d11486
Siddhant Gupta <siddhant.c.gupta@accenture.com>
2021-12-24 18:45:55 +0200

    Fix dev server

--- app/service/mdx.server.ts
+++ app/service/mdx.server.ts
@@ -23,7 +23,7 @@ export async function getMdxPagesInDirectory<T extends ContentCommonData>(
 
 export async function getMdxDirList(contentDir: string) {
   const dirPath = join(CONTENT_PATH, contentDir)
-  const dirList = !__IS_DEV__
+  const dirList = __IS_DEV__
     ? readDirList(dirPath)
     : await downloadDirList(dirPath)
 
@@ -37,7 +37,7 @@ export async function getMdxPage<T extends ContentCommonData>(
   path: string,
   id: string,
 ): Promise<PageContent<T>> {
-  const page = !__IS_DEV__ ? readFile(path) : await downloadFile(path)
+  const page = __IS_DEV__ ? readFile(path) : await downloadFile(path)
   const source = replaceFilePathsInPage(page, path)
   const { code, frontmatter } = await bundleMDX<T>({ source })
 
