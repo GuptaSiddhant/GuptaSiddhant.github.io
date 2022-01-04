731ea1aae171a05cc84589493e19a36870e8da4f
Siddhant Gupta <siddhant.c.gupta@accenture.com>
2022-01-05 01:55:53 +0200

    Move functions to root

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-const functions = require("firebase-functions");
-const express = require("express");
-const compression = require("compression");
-const morgan = require("morgan");
-const { createRequestHandler } = require("@remix-run/express");
+const functions = require("firebase-functions")
+const express = require("express")
+const compression = require("compression")
+const morgan = require("morgan")
+const { createRequestHandler } = require("@remix-run/express")
 
-exports.app = functions.https.onRequest(createApp());
+exports.app = functions.https.onRequest(createApp())
 
 function createApp() {
-  let app = express();
-  app.use(compression());
+  let app = express()
+  app.use(compression())
   app.use(
     express.static("public", {
       immutable: true,
       maxAge: "1y",
-    })
-  );
-  app.use(morgan("tiny"));
-  app.all("*", createRequestHandler({ build: require("./build") }));
-  return app;
+    }),
+  )
+  app.use(morgan("tiny"))
+  app.all("*", createRequestHandler({ build: require("./build") }))
+  return app
 }
 
 /**
