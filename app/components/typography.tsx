76c32b1e1e3297392746dd84faa852aed132ed2a
Siddhant Gupta <siddhant.c.gupta@accenture.com>
2022-02-12 00:54:19 +0530

    Update typography

--- app/components/typography.tsx
+++ app/components/typography.tsx
@@ -12,9 +12,18 @@ export function H1({ className, ...props }: PropsWithChildren) {
 
 export function H2({ className, ...props }: PropsWithChildren) {
   return (
-    <h1
+    <h2
       {...props}
       className={clsx("text-5xl font-bold leading-tight", className)}
+    />
+  )
+}
+
+export function H3({ className, ...props }: PropsWithChildren) {
+  return (
+    <h3
+      {...props}
+      className={clsx("text-4xl font-bold leading-tight", className)}
     />
   )
 }
